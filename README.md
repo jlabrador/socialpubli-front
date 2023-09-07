feddf63930be25a728440735d9f78eb55960a217
jospito <jose.labrador.gonzalez@gmail.com>
2023-09-07 10:48:37 +0200

    Primer commit del proyecto

---
+++ README.md
@@ -0,0 +1,39 @@
+Prueba Técnica SocialPubli Frontend
+========================
+
+La prueba técnica del frontend de "SocialPubli" consiste en una pantalla con una
+tabla que muestra el listado de los personajes de star wars consumidos a través de 
+un un ep expuesto por una api en el backend.
+
+La prueba se ha con VUE 3.
+
+Requerimientos
+------------
+
+  * node 16 o superior
+  * npm 9.6
+
+Instalación
+------------
+
+Ejecutar en el directorio raíz de la aplicación el siguiente comando:
+
+```bash
+$ npm run dev
+```
+
+Uso
+-----
+
+La aplicación será disponible a través de la url:
+
+http://localhost:5173/
+
+Contacto
+-----
+
+Cualquier duda de instalación o uso de la aplicación la puedes consultar en 
+los siguientes contactos:
+
+* Télefono: 655727029
+* Correo: jose.labrador.gonzalez@gmail.com
---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import DataTable from 'datatables.net-vue3';
+import DataTablesCore from 'datatables.net';
+
+DataTable.use(DataTablesCore);
+
+const columns = [
+  { data: 'name' },
+  { data: 'height' },
+  { data: 'mass' },
+  { data: 'hair_color' },
+  { data: 'skin_color' },
+  { data: 'eye_color' },
+];
+</script>
+
+<template>
+  <div>
+    <h1>Prueba técnica SocialPubli</h1>
+    <h2>Listado de personajes de Star Wars</h2>
+
+    <DataTable
+        :columns="columns"
+        ajax="http://api.socialpubli.local:8888/api/people"
+        class="display"
+        width="100%"
+    >
+      <thead>
+      <tr>
+        <th>Name</th>
+        <th>Height</th>
+        <th>Mass</th>
+        <th>Hair color</th>
+        <th>Skin Color</th>
+        <th>Eye Color</th>
+      </tr>
+      </thead>
+      <tfoot>
+      <tr>
+        <th>Name</th>
+        <th>Position</th>
+        <th>Office</th>
+        <th>Extn.</th>
+        <th>Start date</th>
+        <th>Salary</th>
+      </tr>
+      </tfoot>
+    </DataTable>
+  </div>
+</template>
+
+<style>
+@import 'datatables.net-dt';
+</style>
